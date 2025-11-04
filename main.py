68ea7068b06d12f9ea024e4dc4cbc60f3879a776
choeqwersdgwetgwasetgsergwseetgsg <choechanhyeogchoe@gmail.com>
2025-11-04 14:14:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ menu=st.selectbox('좋아하는 음식을 선택해 주세요',['치킨','볶음
 if st.button('인삿말 생성'):
   st.info(name+'님 안녕하세요!')
   st.warning(menu+'을(를) 좋아하시나봐요!, 저도 좋아해요')
-  st.erroe('반갑습니다!!!')
+  st.error('반갑습니다!!!')
   st.balloons()
